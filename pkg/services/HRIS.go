e5c3e866c9337159409bb2b62d52f25332e979cf
Jhoan_Grajales <jhoan@venturit.com>
2022-07-28 07:27:39 -0500

    create HRIS

--- pkg/services/HRIS.go
+++ pkg/services/HRIS.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 	"fmt"
+	"klickhr-hris/pkg/constants"
 	"klickhr-hris/pkg/db"
 	"klickhr-hris/pkg/models"
 	"klickhr-hris/pkg/pb"
 	"klickhr-hris/pkg/utils"
 	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -28,22 +30,48 @@ func (s *Server) UploadHRIS(ctx context.Context, req *pb.UploadHRISRequest) (*pb
 	HRIS.ImportType = int(req.ImportType)
 	HRIS.RunType = int(req.RunType)
 	HRIS.OrganizationLevelId = int(req.OrganizationId)
-
-	fmt.Println("uoload HRIS service")
-
-	/*if result := s.H.DB.Create(&HRIS); result.Error != nil {
+	HRIS.FileUrl = "./" + req.FileName
+	HRIS.FileId = req.FileName
+	HRIS.UserID = 1 // get user by token
+	HRIS.ImportDate = time.Now()
+	HRIS.Status = constants.HRIS_STATUS_ON_HOLD
+	//create row
+	if result := s.H.DB.Create(&HRIS); result.Error != nil {
 		return &pb.UploadHRISResponse{
 			Status: http.StatusConflict,
 			Error:  result.Error.Error(),
 		}, nil
 	}
 
+	go executeHRIS(HRIS)
+
 	return &pb.UploadHRISResponse{
 		Status: http.StatusCreated,
 		Error:  "",
-	}, nil */
+	}, nil
+}
+
+func executeHRIS(HRIS models.HRIS) {
+	switch HRIS.RunType {
+	case constants.HRIS_RUN_TYPE_APPEND:
+		fmt.Println("APPEND")
+	case constants.HRIS_RUN_TYPE_FULL:
+		fmt.Println("FULL")
+	case constants.HRIS_RUN_TYPE_VALIDATE:
+		fmt.Println("VALIDATE")
+	}
+}
+
+func (s *Server) GetAllHRIS(ctx context.Context, req *pb.UploadHRISRequest) (*pb.UploadHRISResponse, error) {
+	var HRIS models.HRIS
+	if result := s.H.DB.Find(&HRIS); result.Error != nil {
+		return &pb.UploadHRISResponse{
+			Status: http.StatusConflict,
+			Error:  result.Error.Error(),
+		}, nil
+	}
 	return &pb.UploadHRISResponse{
 		Status: http.StatusCreated,
-		Error:  "aaa",
+		Error:  "",
 	}, nil
 }
--- pkg/utils/file_handler.go
+++ pkg/utils/file_handler.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 )
@@ -11,4 +12,22 @@ func SaveFile(fileName string, fileBytes []byte) error {
 		return err
 	}
 	return nil
+}
+
+func ReadCSVData(fileName string) ([][]string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	// skip first line
+	if _, err := r.Read(); err != nil {
+		return [][]string{}, err
+	}
+	records, err := r.ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+	return records, nil
 }
